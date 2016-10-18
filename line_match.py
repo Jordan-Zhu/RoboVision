89fa20034c1f6f1987cb25eab711e01f6579b1e4
Jordan Zhu <qwertybot1@gmail.com>
2016-10-18 16:57:54 -0400

    Modified line_match

--- line_match.py
+++ line_match.py
@@ -7,38 +7,50 @@ from distance2d import distance2d
 # Output: ListPair
 def line_match(LineInteresting, P):
 
-    rowsize = LineInteresting.shape[0]
+    rowsize = LineInteresting.shape[0] - 1
     m_mat = np.zeros((rowsize, rowsize))
     d_mat = np.zeros((rowsize, rowsize))
-    ListPair = [0, 0]
+    ListPair = []
 
-    i = 0
-    cnt = 1
+    # i = 0
+    # cnt = 0
 
-    while i <= rowsize:
-        if LineInteresting[i, 4] > P["Cons_Lmin"]:
+    for i in range(0, rowsize):
+        # If the length of the line is larger than min
+        if LineInteresting[i, 4] > int(P["Cons_Lmin"]):
             j = i + 1
-            while j <= rowsize:
-                if LineInteresting[j, 4] > P["Cons_Lmin"]:
-                    if abs(LineInteresting[i, 6] - LineInteresting[j, 6]) <= P["Cons_Lmin"] or (abs(LineInteresting(i,7)-LineInteresting(j,7))>= (180-P["Cons_AlphaD"])):
+            for j in range(j, rowsize):
+                # If length of line is larger than min
+                if LineInteresting[j, 4] > int(P["Cons_Lmin"]):
+                    # If it satisfies the slope constraint
+                    if abs(LineInteresting[i, 6] - LineInteresting[j, 6]) <= int(P["Cons_AlphaD"]) or (abs(LineInteresting[i,6] - LineInteresting[j,6]) >= (180-int(P["Cons_AlphaD"]))):
+                        # print("i =", i, "j =", j)
                         m_mat[i, j] = 1
                         d = distance2d(LineInteresting[i, :], LineInteresting[j, :])
                         d_mat[i, j] = d
-                        if d < P["Cons_Dmax"] and d > P["Cons_Dmin"]:
+                        # If it satisfies the maximum distance
+                        if d < int(P["Cons_Dmax"]) and d > int(P["Cons_Dmin"]):
                             flag_overlap = check_overlap(LineInteresting[i, :], LineInteresting[j,:])
+                            # print("Flag overlap =", flag_overlap)
                             if flag_overlap:
                                 flag_relative = relative_pos(LineInteresting[i, :],LineInteresting[j,:])
+
                                 if flag_relative:
-                                    ListPair[cnt, :] = [LineInteresting[i, 7], LineInteresting[j, 7]]
-                                    cnt += 1
+                                    # print(LineInteresting[i, 7], " + ", LineInteresting[j, 7])
+                                    # Append this line to ListPair
+                                    # print("Lines ", i, "and ", j, "were chosen")
+                                    ListPair.append([LineInteresting[i, 7], LineInteresting[j, 7]])
+                                    # cnt += 1
                                 # end if
                             # end if
                         # end if
-                    j += 1
+                    # j += 1
                     # end if
             # end while
         # end if
-    i += 1
+        # i += 1
+        # print("i =", i)
     # end while
+
 
     return ListPair
