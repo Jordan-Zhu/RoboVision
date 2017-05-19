e074782b9354418d1dd6265c18b05d4bce914f04
Jordan Zhu <qwertybot1@gmail.com>
2017-05-19 16:58:00 -0400

    Completed writing & testing findendsjunctions.

---
+++ Unit Tests/test_junctions.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from python.findendsjunctions import findendsjunctions
+from timeit import default_timer as timer
+
+
+if __name__ == '__main__':
+    im = cv2.imread('junc.png')
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    start = timer()
+    rj, cj, re, ce = findendsjunctions(gray)
+    end = timer()
+    print('time elapsed:', end - start, 'seconds')
+
+    junc = []
+    for i in range(len(rj)):
+        junc.append([cj[i], rj[i]])
+
+    ends = []
+    for i in range(len(re)):
+        ends.append([ce[i], re[i]])
+
+    height = im.shape[0]
+    width = im.shape[1]
+    blank_image = np.zeros((height, width, 3), np.uint8)
+
+    for i, e in enumerate(junc):
+        x1 = int(e[0])
+        y1 = int(e[1])
+        cv2.line(im, (x1, y1), (x1, y1), (0, 255, 0), 1)
+
+    for i, e in enumerate(ends):
+        x1 = int(e[0])
+        y1 = int(e[1])
+        cv2.line(im, (x1, y1), (x1, y1), (0, 0, 255), 1)
+
+    cv2.namedWindow('Junctions and Ends', cv2.WINDOW_NORMAL)
+    cv2.imshow('Junctions and Ends', im)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
--- python/trackedge.py
+++ python/trackedge.py
@@ -3,53 +3,52 @@ import math
 
 
 def trackedge(rstart, cstart, edge_no, r2, c2, avoidJunctions):
-	edge_points = [rstart, cstart]	# Start a new list for this edge.
-	EDGEIM[rstart, cstart] = -edge_no  # Edge points in the iamge are 
-									   # encoded by -ve of their edgeNo.
+    edge_points = [rstart, cstart]  # Start a new list for this edge.
+    EDGEIM[rstart, cstart] = -edge_no  # Edge points in the iamge are
+                                        # encoded by -ve of their edgeNo.
 
-	# If the second point has been supplied, add it to the track
-	# and set the path direction.
-	if r2 && c2 != -1:
-		edge_points = [edge_points, r2, c2]
-		EDGEIM[r2, c2] = -edge_no
+    # If the second point has been supplied, add it to the track
+    # and set the path direction.
+    if r2 & & c2 != -1:
+        edge_points = [edge_points, r2, c2]
+        EDGEIM[r2, c2] = -edge_no
 
-		# Initialize direction vector of path and set the current point on 
-		# the path
-		dirn = unitvector([r2 - rstart, c2 - cstart])
-		row = r2
-		col = c2
-		preferred_dir = 1
-	else:
-		dirn = [0, 0]
-		row = rstart
-		col = cstart
+        # Initialize direction vector of path and set the current point on
+        # the path
+        dirn = unitvector([r2 - rstart, c2 - cstart])
+        row = r2
+        col = c2
+        preferred_dir = 1
+    else:
+        dirn = [0, 0]
+        row = rstart
+        col = cstart
 
-	
-	# Find all pixels we could link to
-	[r_avail, c_avail, r_junc, c_junc] = availablepixels(row, col, edge_no)
+    # Find all pixels we could link to
+    [r_avail, c_avail, r_junc, c_junc] = availablepixels(row, col, edge_no)
 
-	while r_avail or r_junc:
-		# First see if we can link to a junction. Choose the junction
-		# that results in a move that is as close as possible to dir. 
-		# If we have no preferred direction, and there is a choice,
-		# link to the closest junction.
-		# 
-		if (r_junc and not avoidJunctions) or (r_junc and not r_avail):
-			# If we have a preferred direction, choose the junction
-			# that results in a move that is as close as possible
-			# to dirn.
-			if preferred_dir:
-				dotp = -math.inf
-				for n in range(1, len(r_junc)):
-					dirna = unitvector([r_junc[n] - r, c_junc[n] - c])
-					dp = dirn * dirna
-					if dp > dotp:
-						dotp = dp
-						rbest = r_junc[n]
-						cbest = c_junc[n]
-						dirnbest = dirna
+    while r_avail or r_junc:
+        # First see if we can link to a junction. Choose the junction
+        # that results in a move that is as close as possible to dir.
+        # If we have no preferred direction, and there is a choice,
+        # link to the closest junction.
+        #
+        if (r_junc and not avoidJunctions) or (r_junc and not r_avail):
+            # If we have a preferred direction, choose the junction
+            # that results in a move that is as close as possible
+            # to dirn.
+            if preferred_dir:
+                dotp = -math.inf
+                for n in range(1, len(r_junc)):
+                    dirna = unitvector([r_junc[n] - r, c_junc[n] - c])
+                    dp = dirn * dirna
+                    if dp > dotp:
+                        dotp = dp
+                        rbest = r_junc[n]
+                        cbest = c_junc[n]
+                        dirnbest = dirna
 
 
 # Normalizes a vector to unit magnitude.
 def unitvector(v):
-	return np.divide(v, np.sqrt(v.dot(v)))
+    return np.divide(v, np.sqrt(v.dot(v)))
