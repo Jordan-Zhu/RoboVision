Fixed averaging points on the contour
was counting the zeroes when determining the average

label_curves.py
@@ -66,6 +66,13 @@ def mask_mean(src, mask):
         return sum(src[np.nonzero(val_mask)]) / num_nonzero
 
 
+def line_mean(img, y, x):
+    points = img[y, x]
+    num_nonzero = cv2.countNonZero(points)
+    # print(num_nonzero, "non zero", np.sum(points), "sum")
+    return np.sum(points) / num_nonzero
+
+
 def remove_points(lp, roi):
     line_mask = np.invert(np.in1d(lp, roi))
     # print(lp, "lp")
@@ -193,7 +200,7 @@ def label_curves(src, list_lines, list_point, contour):
 
         if line[10] == 12:
             y, x = np.unravel_index([list_point[i]], src.shape, order='F')
-            mean_lp = np.mean(src[y, x])
+            mean_lp = line_mean(src, y, x)
             label = label_convexity(mean_lp, mean_p, mean_n)
             print(label, "curv label")
         elif line[10] == 13 or line[10] == 14:

main.py
@@ -215,7 +215,7 @@ if __name__ == '__main__':
 
         # Remove extra dimensions from data
 
-        res = lineseg(edgelist, tol=2)
+        res = lineseg(edgelist, tol=5)
 
         seglist = []
 
@@ -260,8 +260,9 @@ if __name__ == '__main__':
             # print("angles\n", LineFeature[:, 6])
 
             Line_new, ListPoint_new, line_merged = merge_lines_v4.merge_lines(LineFeature, ListPoint, 20, im_size)
-            print(line_merged, "merged")
-            print("angles\n", Line_new[:, 6])
+            # print(line_merged, "merged")
+            # print("angles\n", Line_new[:, 6])
+
             # Line_new = LineFeature
             # ListPoint_new = ListPoint
 
@@ -438,3 +439,4 @@ if __name__ == '__main__':
             print('List pair:', list_pair)
             blank_im = np.zeros((height, width, 3), np.uint8)
             util.draw_listpair(list_pair, line_new, final_im)
+        cv2.imshow("ALL THE PAIRS", final_im)